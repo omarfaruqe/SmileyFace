129b724fb858b983f1310d2f4fe549ab5b5a51ab
Omar Faruqe <faruqe@Omars-MacBook-Pro.local>
2015-06-05 01:10:50 -0400

    initial commit

---
+++ Smiley/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Smiley
+//
+//  Created by Omar Faruqe on 2015-06-04.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
